9d249018eded0ea382c3248d77f66fda0fb70b1a
HayaLabib <haya.moh07@gmail.com>
2024-04-20 16:40:24 +0200

    User class created

---
+++ src/index2.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(userDetails) {
+        this.id = userDetails.id;
+        this.firstName = userDetails.firstName;
+        this.lastName = userDetails.lastName;
+        this.maidenName = userDetails.maidenName;
+        this.age = userDetails.age;
+        this.gender = userDetails.gender;
+        this.email = userDetails.email;
+        this.phone = userDetails.phone;
+        this.username = userDetails.username;
+        this.password = userDetails.password;
+        this.birthDate = userDetails.birthDate;
+        this.image = userDetails.image;
+        this.bloodGroup = userDetails.bloodGroup;
+        this.height = userDetails.height;
+        this.weight = userDetails.weight;
+        this.eyeColor = userDetails.eyeColor;
+        this.hair = userDetails.hair;
+        this.domain = userDetails.domain;
+        this.ip = userDetails.ip;
+        this.address = userDetails.address;
+        this.macAddress = userDetails.macAddress;
+        this.university = userDetails.university;
+        this.bank = userDetails.bank;
+        this.company = userDetails.company;
+        this.ein = userDetails.ein;
+        this.ssn = userDetails.ssn;
+        this.userAgent = userDetails.userAgent;
+        this.crypto = userDetails.crypto;
+    }
+}
